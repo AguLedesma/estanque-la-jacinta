8a6a1c7e0dee799b710f9628c60b50d72af1275e
AguLedesma <113394799+AguLedesma@users.noreply.github.com>
2022-10-24 23:55:29 -0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Vivero de planatas acuaticas, nenufares, lotos">
+    <meta name="keywords"  content="nenufares, lotos, colocacias, platas acuaticas">
+    <link rel="stylesheet" href="./CSS/estilo.css">
+    <link rel="shortcut icon" href="./Imagenes/Icono nuevo.png">
+    <title>Estanque La Jacinta | Error 404</title>
+</head>
+<body>
+    <div class="container-404">
+        <h1>
+            Error 404
+        </h1>
+        <p>
+         La página solicitada no se encuentra disponible, disculpa las molestias y disfruta de la vista.
+        </p>
+        <a href="index.html" class="boton">
+            Volver al Inicio
+        </a>
+        
+    </div>
+</body>
+</html>
